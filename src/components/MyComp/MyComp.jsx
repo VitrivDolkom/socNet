01ec2203ff63d97bcb45a3f53c51112d59d55c81
Dmitriy Volkov <vdy0555@gmail.com>
2022-11-11 13:31:35 +0700

    configured scss-modules, folders structure, paths

---
+++ src/components/MyComp/MyComp.jsx
@@ -0,0 +1,11 @@
+import s from "./s.module.scss";
+
+const MyComp = () => {
+    return (
+        <div className={s.block}>
+            Component
+        </div>
+    );
+}
+
+export default MyComp;
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
     css: {
         preprocessorOptions: {
             scss: {
                 additionalData: `
                 @import "./src/styles/_vars.scss";
-            `
-            }
+                `,
+            },
+
+        },
+        modules: {
+            localsConvention: "camelCase",
         }
-    }
+    },
 })
