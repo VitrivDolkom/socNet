866acf3d7d0af99c7c9a621b2c87413c68f4073a
Dmitriy Volkov <vdy0555@gmail.com>
2022-11-14 22:31:50 +0700

    added basic layout

--- src/pages/Messenger/Messenger.jsx
+++ src/pages/Messenger/Messenger.jsx
@@ -3,7 +3,16 @@ import s from "./s.module.scss";
 const Messenger = () => {
     return (
         <div className={`${s.wrap} `}>
-            Messenger
+            <ul className={`${s.userList}`}>
+                <li>Dima</li>
+                <li>Andrey</li>
+                <li>Vika</li>
+                <li>Kirill</li>
+            </ul>
+            <div className={`${s.dialog}`}>
+                Hi
+                How are you?
+            </div>
         </div>
     );
 }
