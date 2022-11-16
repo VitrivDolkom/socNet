21e9cb70c1770dca47fa505d0465c68c36be2a1e
Dmitriy Volkov <vdy0555@gmail.com>
2022-11-16 21:35:48 +0700

    added ability to add and delete post

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,14 +1,30 @@
+import { PostsContext } from "@/pages/Profile/Profile";
+import { useContext } from "react";
 import Post from "../Post/Post";
 import s from "./s.module.scss";
 
+
+
 const Posts = () => {
+    const { postsList, setPostsList } = useContext(PostsContext);
+
+
+    const removePost = (i) => {
+        setPostsList(prev => {
+            return prev.filter((el, index) => index !== i);
+        });
+    }
+
     return (
         <div className="">
             <ul className={s.list}>
-                <Post />
-                <Post />
-                <Post />
-                <Post />
+                {postsList.map((post, index) => {
+                    return (<Post
+                        key={index}
+                        text={post}
+                        index={index}
+                        deletePost={(i) => removePost(i)} />);
+                })}
             </ul>
         </div>
     );
